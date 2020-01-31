0617cca865af152916a64d6207ba7961c907c8e8
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-01-31 23:19:14 +1100

    Adds organizations

--- pulumi/config/organization.py
+++ pulumi/config/organization.py
@@ -3,3 +3,8 @@ import pulumi
 config = pulumi.Config()
 
 aws_organization_id = "o-syu4m3phrj"
+aws_organization_root_id = "r-b2xr"
+
+aws_organization_account_name = "freelancer1001"
+aws_organization_account_email = f"root-{aws_organization_account_name}@aws.nuage.studio"
+aws_organization_account_user = "blabla"
