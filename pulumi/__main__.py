08d17bccff91dd41767b0cfd5a684a0c8f5523aa
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-02-05 00:01:42 +1100

    Reverts register_to_work_mail call on WorkMail creation as this out of scope

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,19 +1,22 @@
-from config.workmail import workmail_organization_id, workmail_domain, workmail_alias_name, workmail_group_name, \
-    workmail_group_email
-from dynamic_providers.workmail.alias import Alias
+from pulumi import export, Config, get_stack
+
 from dynamic_providers.workmail.group import Group
-from pulumi import export, Config
+
+from dynamic_providers.workmail.alias import Alias
+from pulumi.runtime import get_root_resource
 
 config = Config()
 
-group = Group(name="WorkMailGroupCreation", organization_id=workmail_organization_id,
-              group_name=workmail_group_name, group_email=workmail_group_email)
+group = Group(name="WorkMailGroupCreation", organization_id='m-b01addc6667743b0b865f62342b5a217',
+              group_name='AWSFreelancersRoot')
 
 export("group_export_group_id", group.group_id)
 
-alias_email = f"root-{workmail_alias_name}@{workmail_domain}"
-alias = Alias(name="WorkMailAliasCreation", group_id=group.group_id,
-              alias_email=alias_email, organization_id=workmail_organization_id)
+""" This method would give a exception if you don't enable the group that you have create that needs to be done manually
+    as in boto3 or aws api there is no way to enable a group """
 
+alias_email = 'root-' + config.get('alias_name') + '@' + config.get('sub_domain')
+alias = Alias(name="WorkMailAliasCreation", group_id=group.group_id,
+              alias_email=alias_email, organization_id='m-b01addc6667743b0b865f62342b5a217')
 export("alias_export_alias_email", alias.alias_email)
 export("alias_export_group_id", alias.group_id)
