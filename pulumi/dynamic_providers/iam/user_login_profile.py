bc20eaf65cb3bf394bb668805ae9eaa5ae91dcc7
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-02-15 20:40:58 +1100

    Fixes update and delete logic for UserLoginProfile resource

--- pulumi/dynamic_providers/iam/user_login_profile.py
+++ pulumi/dynamic_providers/iam/user_login_profile.py
@@ -8,6 +8,14 @@ required_props = ['username', 'password']
 
 
 def client_with_session(name: str, role_arn):
+    """
+    Creates boto3 resource with assuming provided role
+
+    :param name:
+    :param role_arn:
+    :return:
+    """
+
     sts = boto3.client('sts')
     response = sts.assume_role(
         RoleArn=role_arn,
@@ -23,6 +31,14 @@ def client_with_session(name: str, role_arn):
 
 
 def client(name: str, props: Any):
+    """
+    Creates boto3 resource from provided props
+
+    :param name:
+    :param props:
+    :return:
+    """
+
     return client_with_session(name, props["assume_role_arn"]) if "assume_role_arn" in props else boto3.resource(name)
 
 
@@ -36,15 +52,29 @@ class AssumeRole:
 class UserLoginProfileProvider(ResourceProvider):
 
     def check(self, _olds: Any, _news: Any):
+        """
+        Checks all required props are set
+
+        :param _olds:
+        :param _news:
+        :return:
+        """
         failures = []
 
         for prop in required_props:
             if prop not in _news:
-                failures.append(CheckFailure(property_='username', reason=f"property {prop} missing"))
+                failures.append(CheckFailure(property_=prop, reason=f"property {prop} missing"))
 
         return CheckResult(inputs=_news, failures=failures)
 
-    def create(self, _news: Any):
+    def create(self, _news: Any) -> CreateResult:
+        """
+        Creates new UserLoginProfile resource
+
+        :param _news:
+        :return:
+        """
+
         iam = client("iam", _news)
         username = _news['username']
         password = _news['password']
@@ -52,9 +82,18 @@ class UserLoginProfileProvider(ResourceProvider):
         login_profile.create(Password=password,
                              PasswordResetRequired=True)
 
-        return CreateResult(username, {**_news, 'password_reset_required': True})
+        return CreateResult(username, outs={**_news, 'password_reset_required': True})
 
-    def update(self, _id: str, _olds: Any, _news: Any):
+    def update(self, _id: str, _olds: Any, _news: Any) -> UpdateResult:
+        """
+        Updates existing UserLoginProfile resource
+
+        :param _id:
+        :param _olds:
+        :param _news:
+        :return:
+        """
+
         iam = client("iam", _news)
         username = _olds["username"]
         password = _news["password"]
@@ -62,21 +101,41 @@ class UserLoginProfileProvider(ResourceProvider):
         login_profile.update(Password=password,
                              PasswordResetRequired=True)
 
-        return UpdateResult()
+        return UpdateResult(outs={**_news, 'password_reset_required': True})
+
+    def diff(self, _id: str, _olds: Any, _news: Any) -> DiffResult:
+        """
+        Checks if an update needed
+
+        :param _id:
+        :param _olds:
+        :param _news:
+        :return:
+        """
+        changes = _olds['username'] == _news['username'] and not _olds['password'] == _news['password']
+        return DiffResult(changes=bool(changes))
 
     def delete(self, _id: str, _olds: Any):
+        """
+        Deletes UserLoginProfile resource
+
+        :param _id:
+        :param _olds:
+        :return:
+        """
+
         iam = client("iam", _olds)
         username = _olds['username']
         login_profile = iam.LoginProfile(user_name=username)
         login_profile.delete()
 
-    def diff(self, _id: str, _olds: Any, _news: Any):
-        changes = _olds['username'] == _news['username'] and not _olds['password'] == _news['password']
-        return DiffResult(changes=bool(changes))
-
 
 class UserLoginProfile(Resource):
+    """
+    IAM UserLoginProfile resource to provide access to AWS Console
+    """
+
     def __init__(self, name, username: Input[str], password: Input[str], assume_role: AssumeRole = None, opts=None):
-        full_args = {'username': username, 'password': password, **vars(assume_role)} if (assume_role) else {
+        full_args = {'username': username, 'password': password, **vars(assume_role)} if assume_role else {
             'username': username, 'password': password}
         super().__init__(UserLoginProfileProvider(), name, full_args, opts)
